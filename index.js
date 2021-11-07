d7a08b583a299a3574c5b44856f733a8ef953c7b
Andres <andres.martinez.ccr@gmail.com>
2021-11-07 15:57:28 -0300

    main

--- index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector("barra--toggle")
-const navMenu = document.querySelector("barra--menu")
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toogle("nav--menu__visible")
-})
